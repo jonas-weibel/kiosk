712db6fa088438f884d7482637be9e625c9014f1
jw <jonas.weibel@outlook.de>
2026-08-21 23:37:03 +0200

    maintenance mode refined

--- maintenance-mode.sh
+++ maintenance-mode.sh
@@ -9,14 +9,26 @@ pkill -f "733-videokiosk-chromium" 2>/dev/null || true
 # Schwarzen Hintergrund entfernen
 pkill -x swaybg 2>/dev/null || true
 
-sleep 1
+while pgrep -f "733-videokiosk-chromium" >/dev/null \
+   || pgrep -x swaybg >/dev/null; do
+    sleep 0.1
+done
 
 # Normalen Desktop wiederherstellen
 pcmanfm --desktop-off 2>/dev/null || true
+
+while pgrep -x pcmanfm >/dev/null; do
+    sleep 0.1
+done
+
 /usr/bin/pcmanfm --desktop >/dev/null 2>&1 &
 
 
 # Panel wiederherstellen
 pkill -x wf-panel-pi 2>/dev/null || true
-sleep 1
+
+while pgrep -x wf-panel-pi >/dev/null; do
+    sleep 0.1
+done
+
 /usr/bin/wf-panel-pi >/dev/null 2>&1 &
