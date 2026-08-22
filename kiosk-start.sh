15ed127bdf9de13fa65443c279fe76bba8a80a9b
jw <jonas.weibel@outlook.de>
2026-08-22 02:07:09 +0200

    Reorganize kiosk application structure

--- kiosk-start.sh
+++ kiosk-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-APP="file://$(realpath "$SCRIPT_DIR/../Web Application/index.html")"
+APP="file://$(realpath "$SCRIPT_DIR/app/index.html")"
 
 PROFILE="$HOME/.config/733-videokiosk-chromium"
 
